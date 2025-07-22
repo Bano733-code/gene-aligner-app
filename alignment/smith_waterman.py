25121b1968f4a79deb557abf5712a76b4ebbc19e
Bano733-code <banor3106@gmail.com>
2025-07-22 14:45:18 +0500

    Create smith_waterman.py

---
+++ alignment/smith_waterman.py
@@ -0,0 +1,34 @@
+def smith_waterman(seq1, seq2, match=1, mismatch=-1, gap=-2):
+    m, n = len(seq1), len(seq2)
+    score = [[0] * (n + 1) for _ in range(m + 1)]
+    max_score = 0
+    max_pos = (0, 0)
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            diag = score[i - 1][j - 1] + (match if seq1[i - 1] == seq2[j - 1] else mismatch)
+            delete = score[i - 1][j] + gap
+            insert = score[i][j - 1] + gap
+            score[i][j] = max(0, diag, delete, insert)
+            if score[i][j] > max_score:
+                max_score = score[i][j]
+                max_pos = (i, j)
+
+    align1, align2 = "", ""
+    i, j = max_pos
+    while score[i][j] != 0:
+        if score[i][j] == score[i - 1][j - 1] + (match if seq1[i - 1] == seq2[j - 1] else mismatch):
+            align1 = seq1[i - 1] + align1
+            align2 = seq2[j - 1] + align2
+            i -= 1
+            j -= 1
+        elif score[i][j] == score[i - 1][j] + gap:
+            align1 = seq1[i - 1] + align1
+            align2 = "-" + align2
+            i -= 1
+        else:
+            align1 = "-" + align1
+            align2 = seq2[j - 1] + align2
+            j -= 1
+
+    return max_score, align1, align2
